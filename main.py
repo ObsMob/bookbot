d0c1b627588239ad0ba1fa30547ab257e38e2c1d
ObsMob <obstinata.mobilitas@gmail.com>
2025-06-08 19:48:52 -0500

    added stats, update main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+from stats import word_count
+
+def main():
+    filepath = "books/frankenstein.txt"
+    num_words = word_count(get_book_text(filepath))
+    print(f"{num_words} words found in the document")
+
+main()
