8b8bf4c12ae07ae84171052b3404b0d5f5589996
ObsMob <obstinata.mobilitas@gmail.com>
2025-06-08 22:24:33 -0500

    added sorted dictionary, decorative print, and sys.argv reference

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+import sys
+    
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    filepath = sys.argv[1]
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
 from stats import word_count
+from stats import character_dict
+from stats import dict_sort
 
-def main():
-    filepath = "books/frankenstein.txt"
+def main(): 
     num_words = word_count(get_book_text(filepath))
-    print(f"{num_words} words found in the document")
-
+    character_count = character_dict(get_book_text(filepath))
+    sorted = dict_sort(character_count)
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {filepath}...")
+    print(f"----------- Word Count ----------\nFound {num_words} total words")
+    print("--------- Character Count -------")
+    for char in sorted:
+        if char["character"].isalpha():
+            print(f"{char["character"]}: {char["value"]}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,22 @@
 def word_count(file_contents):
     word_list = file_contents.split()
     return len(word_list)
+
+def character_dict(file_contents):
+    characters = {}
+    lowercased = file_contents.lower()
+    for lowercase in lowercased:
+        if lowercase in characters:
+            characters[f"{lowercase}"] += 1
+        else:
+            characters[f"{lowercase}"] = 1
+    return characters
+
+def dict_sort(character_dict):
+    def sort_value(sorted):
+        return sorted["value"]
+    sorted = []
+    for character in character_dict:
+        sorted.append({"character": character, "value": character_dict[character]})
+    sorted.sort(reverse=True, key=sort_value)
+    return sorted
